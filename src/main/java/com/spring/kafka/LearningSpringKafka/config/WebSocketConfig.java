b68da65c2c2d8ebc69f08ef2cae3dcaaa2e757c6
Ajmal Hussain <ajmalhsn@hotmail.com>
2026-09-07 15:24:46 -0400

    updated websocket connection point

--- src/main/java/com/spring/kafka/LearningSpringKafka/config/WebSocketConfig.java
+++ src/main/java/com/spring/kafka/LearningSpringKafka/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5173") // Allow requests from the React frontend
+                .setAllowedOrigins("https://reactkafka-764300521353.europe-west1.run.app")
                 .withSockJS();
     }
 }
